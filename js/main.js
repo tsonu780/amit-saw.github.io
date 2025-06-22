b6ac0d38052719b2d1225da3e7f71574eab0de4d
suthakur <sonu.thakur@skillsoft.com>
2025-06-22 19:39:29 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     var navbar = document.getElementById('navbar').querySelector('ul');
     navbar.classList.toggle('active');
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
